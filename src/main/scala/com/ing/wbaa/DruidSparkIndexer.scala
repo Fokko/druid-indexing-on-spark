ae8c9bf7b9c395c63e2515b57f9634d54b40ccc0
Stijn Koopal <stijn.koopal@gmail.com>
2018-05-07 15:40:46 +0200

    Add segment estimation

--- src/main/scala/com/ing/wbaa/DruidSparkIndexer.scala
+++ src/main/scala/com/ing/wbaa/DruidSparkIndexer.scala
@@ -21,17 +21,28 @@ object DruidSparkIndexer extends App {
 
   val inputDf = spark
     .read
-    .json("/Users/fokkodriesprong/Desktop/docker-druid/ingestion/wikiticker-2015-09-12-sampled.json")
+    .json("/Users/stijnkoopal/Documents/projects/ing/experimentation-day/druid-indexing-on-spark/wikiticker-2015-09-12-sampled.json")
+
+  val targetSizeInBytes = 1 * 1024 * 1024
+
+  val millisPerHour = 60 * 60 * 1000
+  val millisPerDay = 24 * millisPerHour
+
+  val bucket = udf((time: String) => (new DateTime(time).getMillis / millisPerHour).floor)
+  val estimateSize = udf((row: Row) => SizeEstimator.estimate(row))
+  val numSegments = udf((sizeInBytes: Long) => (sizeInBytes / targetSizeInBytes).ceil)
 
   val rolledUpDf = inputDf.withColumn("__time", bucket('time))
     .withColumn("__size", estimateSize(struct(inputDf.columns.map(col): _*)))
     .cache()
 
-  rolledUpDf.groupBy("__time")
+  val partitionsDf = rolledUpDf.groupBy("__time")
     .sum("__size")
     .withColumn("num_segments", numSegments(col("sum(__size)")))
-    .show
-
+//
+//  partitionsDf.join(rolledUpDf, "__time")
+//    .withColumn("bucket", "num_segments".)
+//
 //  df.map(row =>{
 //    import io.druid.indexer.InputRowSerde
 //    val row = new SparkBasedInputRow(row)
